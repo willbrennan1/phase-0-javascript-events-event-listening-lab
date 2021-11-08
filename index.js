29bd140db555b8a00eae1b3ed519991aac63710c
Will Brennan <willbrennan314@gmail.com>
2021-11-08 11:28:18 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// let input = document.getElementById('input');
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
 }
+
+// function clickAlert() {
+
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
